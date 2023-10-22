100ca40d4affe7a7d60a41ff4b476890d2c25b4e
rnikam59 <rnikam59@gmail.com>
2023-10-22 15:01:51 +0530

    Main file added.

---
+++ src/main/java/com/health_tracker_record/reciver/ReciverApplication.java
@@ -0,0 +1,13 @@
+package com.health_tracker_record.reciver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReciverApplication.class, args);
+	}
+
+}
